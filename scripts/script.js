6151efef9219a68884c94eda171a68f748ad3cce
Alexandr <alexandrgorr@gmail.com>
2021-10-08 05:42:58 +0300

    update my settings with pretties

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
-const title;
-const screens;
-const screenPrice;
-const rollback;
-const fullPrice;
-const adaptive;
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
 
-alert('text for search');
+alert("text for search");
 
-console.log('my work');
+console.log("my work");
