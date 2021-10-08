763b64caf45c4cc025169b0f59d000bef9391678
Alexandr <alexandrgorr@gmail.com>
2021-10-08 06:13:27 +0300

    methods and properties les02

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,18 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "les02";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 12;
+let rollback = 6;
+let fullPrice = 55000;
+let adaptive = true;
 
-alert("text for search");
+//metods and properties
 
-console.log("my work");
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log(
+    "Стоимость верстки экранов " + screenPrice + " юани.",
+    "Стоимость разработки сайта  " + fullPrice + " долларов!"
+);
+console.log(screens.toLocaleLowerCase().split(" "));
+
+console.log(Math.round(fullPrice * (rollback / 100)));
