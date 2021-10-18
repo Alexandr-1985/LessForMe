ef1a6c6d1d23cf77f3ace7cc3640ce5fa1b74857
Alexandr <alexandrgorr@gmail.com>
2021-10-19 02:51:15 +0300

    functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,65 +1,73 @@
 "use strict";
 
-let title, screens, screenPrice, fullPrice, adaptive;
-let rollback = 6;
-
-//metods and properties
-console.log(typeof title, typeof fullPrice, typeof adaptive);
-
-console.log(
-    "Стоимость верстки экранов " + screenPrice + " юани.",
-    "Стоимость разработки сайта  " + fullPrice + " долларов!"
-);
-
-console.log(Math.round(fullPrice * (rollback / 100)));
-
-title = prompt("Как называется ваш проект?");
-console.log(title);
-
-screens = prompt(
+let title = prompt("Как называется ваш проект?");
+let screens = prompt(
     "Какие типы экранов нужно разработать?",
     "Простые, Сложные, Интерактивные"
 );
-console.log(screens);
-
-console.log(screens.length);
-console.log(screens.toLocaleLowerCase().split(" "));
-
-screenPrice = +prompt("Сколько будет стоить данная работа?", 12000);
-console.log(screenPrice);
-
-adaptive = confirm("Нужен ли адаптив на сайте?");
-console.log(adaptive);
-
+let screenPrice = +prompt("Сколько будет стоить данная работа?", 12000);
+let adaptive = confirm("Нужен ли адаптив на сайте?");
 let service1 = prompt("Какой дополнительный тип услуги нужен?", " ");
 let servicePrice1 = +prompt("Сколько это будет стоить?", " ");
 let service2 = prompt("Какой дополнительный тип услуги нужен?", " ");
 let servicePrice2 = +prompt("Сколько это будет стоить?", " ");
+let rollback = 6;
+let fullPrice;
+let servicePercentPrice;
+let allServicePrices;
 
-//итоговую стоимость работы
-fullPrice = +(screenPrice + servicePrice1 + servicePrice2);
-console.log("итоговую стоимость работы: ", fullPrice);
+const showTypeOf = (variable) => {
+    console.log(variable, typeof variable);
+};
 
-//занести в нее итоговую стоимость за вычетом отката посреднику
-let n = "Откат посреднику";
-n = 3000;
-let servicePercentPrice = console.log(Math.ceil(fullPrice - n));
+//Функция возвращает сумму всех дополнительных услуг
+const getAllServicePrices = function() {
+    return servicePrice1 + servicePrice2;
+};
+
+//Функция возвращает сумму стоимости верстки и стоимости дополнительных услуг
+const getFullPrice = function() {
+    return screenPrice + allServicePrices;
+};
+
+//Функция возвращает title меняя его таким образом: первый символ с большой буквы, остальные с маленькой".
+const getTitle = function() {
+    return title.trim()[0].toUpperCase() + title.trim().substr(1).toLowerCase();
+    // return title.trim().toLocaleLowerCase().split(" ");
+};
+
+//Функция возвращает итоговую стоимость за вычетом процента отката.
+const getServicePercentPrices = function() {
+    return fullPrice - fullPrice * (rollback / 100);
+};
 
 //Конструктор условий
-if (fullPrice > 30000) {
-    console.log("Даем скидку в 10%");
-} else if (15000 < fullPrice && fullPrice < 30000) {
-    console.log("Даем скидку в 5 %");
-} else if (15000 > fullPrice && fullPrice > 0) {
-    console.log("Скидка не предусмотрена");
-} else if (fullPrice < 0) {
-    console.log("Что то пошло не так");
-} else if (fullPrice === 30000) {
-    console.log("Вам он точно нужен?");
-} else if (fullPrice === 15000) {
-    console.log("Оптимальная цена");
-} else if (fullPrice === 0) {
-    console.log("Указать цену");
-} else {
-    console.log("обратиться к рукаводству");
-}
+const getRollbackMessage = function(price) {
+    if (price >= 30000) {
+        return "Даем скидку в 10%";
+    } else if (price >= 15000 && price < 30000) {
+        return "Даем скидку в 5 %";
+    } else if (price >= 0 && price < 15000) {
+        return "Скидка не предусмотрена";
+    } else {
+        return "Что то пошло не так";
+    }
+};
+
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+title = getTitle();
+servicePercentPrice = getServicePercentPrices();
+
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
+
+console.log(typeof title, typeof fullPrice, typeof adaptive);
+console.log(screens.length);
+console.log(servicePercentPrice);
+console.log(getRollbackMessage(fullPrice));
+console.log(
+    "Стоимость верстки экранов " + screenPrice + " юани.",
+    "Стоимость разработки сайта  " + fullPrice + " долларов!"
+);
