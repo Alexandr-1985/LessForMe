c5ae5150fe07ef99f3e6e8f856b4092c00e3d74c
Alexandr <alexandrgorr@gmail.com>
2021-10-22 04:22:49 +0300

    work with do while and isNaN isNumber Finit

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,35 @@
 "use strict";
 
-let title = prompt("Как называется ваш проект?");
-let screens = prompt(
-    "Какие типы экранов нужно разработать?",
-    "Простые, Сложные, Интерактивные"
-);
-let screenPrice = +prompt("Сколько будет стоить данная работа?", 12000);
-let adaptive = confirm("Нужен ли адаптив на сайте?");
-let service1 = prompt("Какой дополнительный тип услуги нужен?", " ");
-let servicePrice1 = +prompt("Сколько это будет стоить?", " ");
-let service2 = prompt("Какой дополнительный тип услуги нужен?", " ");
-let servicePrice2 = +prompt("Сколько это будет стоить?", " ");
+let title;
+let screens;
+let screenPrice;
+let adaptive;
+let service1;
+let service2;
 let rollback = 6;
 let fullPrice;
 let servicePercentPrice;
 let allServicePrices;
+
+//isFinite - проверка на число
+const isNumber = function(num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+};
+const asking = function() {
+    title = prompt("Как называется ваш проект?");
+    screens = prompt(
+        "Какие типы экранов нужно разработать?",
+        "Простые, Сложные, Интерактивные"
+    );
+    screenPrice = prompt("Сколько будет стоить данная работа?");
+
+    //while (isNaN(screenPrice) || screenPrice.trim() === "" || screenPrice === null)
+    do {
+        screenPrice = prompt("Сколько будет стоить данная работа?");
+    } while (!isNumber(screenPrice));
+
+    adaptive = confirm("Нужен ли адаптив на сайте?");
+};
 
 const showTypeOf = (variable) => {
     console.log(variable, typeof variable);
@@ -22,7 +37,23 @@ const showTypeOf = (variable) => {
 
 //Функция возвращает сумму всех дополнительных услуг
 const getAllServicePrices = function() {
-    return servicePrice1 + servicePrice2;
+    let sum = 0;
+
+    for (let i = 0; i < 2; i++) {
+        let sumService = 0;
+        if (i === 0) {
+            service1 = prompt("Какой дополнительный тип услуги нужен?", " ");
+        }
+        if (i === 1) {
+            service2 = prompt("Какой дополнительный тип услуги нужен?", " ");
+        }
+        do {
+            sumService = prompt("Сколько это будет стоить?");
+        } while (!isNumber(sum));
+        sum += +sumService;
+    }
+
+    return sum;
 };
 
 //Функция возвращает сумму стоимости верстки и стоимости дополнительных услуг
@@ -54,6 +85,7 @@ const getRollbackMessage = function(price) {
     }
 };
 
+asking();
 allServicePrices = getAllServicePrices();
 fullPrice = getFullPrice();
 title = getTitle();
@@ -62,6 +94,8 @@ servicePercentPrice = getServicePercentPrices();
 showTypeOf(title);
 showTypeOf(screenPrice);
 showTypeOf(adaptive);
+
+console.log("allServicePrices", allServicePrices);
 
 console.log(typeof title, typeof fullPrice, typeof adaptive);
 console.log(screens.length);
