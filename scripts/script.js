169206e98b8f6d493b032bac8987be18888252ca
Alexandr <alexandrgorr@gmail.com>
2021-10-31 02:44:30 +0300

    for exam

--- scripts/script.js
+++ scripts/script.js
@@ -1,89 +1,195 @@
 "use strict";
 
-//Работа с Index1.html
-const books = document.querySelectorAll(".books"),
-    arrBook = document.querySelectorAll(".book");
+//console.dir(document);
 
-console.log(books);
-console.log(arrBook);
+//Получить заголовок "Калькулятор верстки"через метод getElementsByTagName.(тэг h1, получить именно элемент, а не коллекцию)
+const title = document.getElementsByTagName("h1")[0];
 
-//Восстановить порядок книг.
-const arr = Object.keys(arrBook).sort((prev, next) => {
-    if (
-        arrBook[prev].firstElementChild.innerText >
-        arrBook[next].firstElementChild.innerText
-    ) {
-        return 1;
-    }
-    if (
-        arrBook[prev].firstElementChild.innerText <
-        arrBook[next].firstElementChild.innerText
-    ) {
-        return -1;
-    }
-});
-for (let i = 0; i < arr.length; i++) {
-    books[0].append(arrBook[arr[i]]);
-}
+//Получить кнопки "Рассчитать" и "Сброс" через метод getElementsByClassName.(класс handler_btn)
+const calcBtn = document.getElementsByClassName("handler_btn")[0];
+const resetBtn = document.getElementsByClassName("handler_btn")[1];
 
-//Заменить картинку заднего фона на другую из папки image
-document.querySelector("body").style.background = "url(./image/open_book.jpg)";
-//document.querySelector('body').style.backgroundImage = ('url(./image/open_book.jpg)');
+// Получить кнопку "+" под выпадающим списком через метод querySelector.(класс screen - btn)
+const plusBtn = document.querySelector(".screen-btn");
 
-//Исправить заголовок в книге 3( Получится - "Книга 3. this и Прототипы Объектов")
-books[0].children[2].querySelector("h2").querySelector("a").textContent =
-    '"Книга 3. this и Прототипы Объектов"';
+//Получить все элементы с классом other - items в две разные переменные.В первую элементы у которых так же присутствует класс percent, во вторую элементы у которых так же присутствует класс number через метод querySelectorAll.
+const otherItemsPercent = document.querySelectorAll(".other-items.percent");
+const otherItemsNumber = document.querySelectorAll(".other-items.number");
 
-//Удалить рекламу со страницы
-document.querySelector(".adv").remove();
+//Получить input type = range через его родителя с классом rollback одним запросом через метод querySelector.
+const inputRange = document.querySelector(".rollback input");
 
-//Восстановить порядок глав во второй и пятой книге (внимательно инспектируйте индексы элементов, поможет dev tools)
-const sortChapterLi = (collection) => {
-    const sortElements = (arr) => {
-        const arrLi = Object.keys(arr).sort((prev, next) => {
-            if (arr[prev].textContent > arr[next].textContent) {
-                return 1;
-            }
-            if (arr[prev].textContent < arr[next].textContent) {
-                return -1;
+//Получить span с классом range - value через его родителя с классом rollback одним запросом через метод querySelector.
+const spanRange = document.querySelector(".rollback .range-value");
+
+//Получить все инпуты с классом total - input справа через метод getElementsByClassName.(класс total - input, получить именно элементы, а не коллекции)
+const total = document.getElementsByClassName("total-input")[0];
+const totalCount = document.getElementsByClassName("total-input")[1];
+const totalCountOther = document.getElementsByClassName("total-input")[2];
+const totalFullCount = document.getElementsByClassName("total-input")[3];
+const totalCountRollback = document.getElementsByClassName("total-input")[4];
+
+/*const mainControlsSelect = document.querySelector(".main-controls__select");
+const viewsSelect = document.createElement("class");
+console.log(viewsSelect);*/
+
+//Получить все блоки с классом screen в изменяемую переменную(let) через метод querySelectorAll(далее мы будем переопределять ее значение)
+const screens = document.querySelectorAll(".screen");
+
+const appData = {
+    title: "",
+    screens: [],
+    screenPrice: 0,
+    adaptive: true,
+    services: {},
+    rollback: 0,
+    fullPrice: 0,
+    servicePercentPrice: 0,
+    servicePricesPercent: 0, //все услуги с процентным соотношение
+    servicePricesNumber: 0, //все услуги с фиксированной суммой
+    servicesPercent: {}, //кладем цену ввиде процента
+    servicesNumbers: {}, //кладем фиксированную стоимость
+
+    //метод будет запускаться во время загрузки страницы и будет запускать метод start
+    init: function() {
+        appData.addTitle();
+
+        //вешаем событие
+        document
+            .getElementsByClassName("handler_btn")
+            .calcBtn.addEventListener("click", appData.start);
+        if (calcBtn.length === "" && calcBtn.length === null) {
+            calcBtn.style.display = "none";
+        }
+        plusBtn.addEventListener("click", appData.addScreenBlock);
+    },
+
+    addTitle: function() {
+        //находим название по тегу
+        // console.log(title.textContent);
+        //меняем название фавикона
+        document.title = title.textContent;
+    },
+
+    start: function() {
+        appData.addScreens();
+        appData.addServices();
+        appData.addPrices();
+        //appData.getServicePercentPrices();
+        //appData.logger();
+        appData.showRange();
+        appData.showResult();
+    },
+
+    showRange: function() {
+        const logger = function(event) {
+            spanRange.textContent = event.target.value;
+        };
+        inputRange.addEventListener("input", logger);
+        // inputRange.addEventListener("change", logger);
+
+        appData.rollback = spanRange.textContent;
+    },
+
+    //метод который выводит все на экран
+    showResult: function() {
+        total.value = appData.screenPrice;
+        totalCount.value = appData.screens;
+        totalCountOther.value =
+            appData.servicePricesPercent + appData.servicePricesNumber;
+        totalFullCount.value = appData.fullPrice;
+        totalCountRollback.value = appData.servicePercentPrice;
+    },
+
+    //метод для заполнения св-ва screens объектами на основе из верстки
+    addScreens: function() {
+        //переопределяем коллекцию перед каждым расчетом
+        const screens = document.querySelectorAll(".screen");
+
+        screens.forEach(function(screen, index) {
+            // console.log(screen);
+            const select = screen.querySelector("select");
+            const input = screen.querySelector("input");
+            //достаем index текст
+            const selectName = select.options[select.selectedIndex].textContent;
+            //console.log(select.value);
+            //console.log(input.value);
+            appData.screens.push({
+                id: index,
+                name: selectName,
+                sumService: +select.value * +input.value,
+                count: input,
+            });
+        });
+        console.log(appData.screens);
+    },
+
+    //перебираем обе коллекции достаем инфу и записать в объект services
+    addServices: function() {
+        otherItemsPercent.forEach(function(item) {
+            const check = item.querySelector("input[type=checkbox]");
+            const label = item.querySelector("label");
+            const input = item.querySelector("input[type = text]");
+
+            if (check.checked) {
+                appData.servicesPercent[label.textContent] = +input.value;
             }
         });
-        let arrNewLi = [];
-        for (let i = 0; i < arrLi.length; i++) {
-            arrNewLi.push(arr[arrLi[i]]);
-        }
-        return arrNewLi;
-    };
 
-    let arrChapters = [];
-    let arrApp = [];
+        otherItemsNumber.forEach(function(item) {
+            const check = item.querySelector("input[type=checkbox]");
+            const label = item.querySelector("label");
+            const input = item.querySelector("input[type = text]");
 
-    const elem = collection.querySelectorAll("li");
-    elem.forEach((elLi) => {
-        if (elLi.textContent.indexOf("Введение") > 1) {
-            collection.insertAdjacentElement("beforebegin", elem[0]);
-        } else if (elLi.textContent.indexOf("Предисловие") > 1) {
-            collection.insertAdjacentElement("beforebegin", elem[1]);
-        } else if (elLi.textContent.indexOf("Глава") > -1) {
-            arrChapters.push(elLi);
-        } else if (elLi.textContent.indexOf("Приложение") > -1) {
-            arrApp.push(elLi);
+            if (check.checked) {
+                appData.servicesNumbers[label.textContent] = +input.value;
+            }
+        });
+    },
+
+    addScreenBlock: function() {
+        //создаем переменную первого элемента в коллекции screens
+        const cloneScreen = screens[0].cloneNode(true);
+        screens[screens.length - 1].after(cloneScreen);
+    },
+
+    //метод будет высчитывать стоимость наших услуг и экранов
+    addPrices: function() {
+        //перебираем типы экранов
+        for (let screen of appData.screens) {
+            appData.screenPrice += +screen.sumService;
         }
-    });
-    arrChapters = sortElements(arrChapters);
-    arrChapters.forEach((elLi) => {
-        collection.append(elLi);
-    });
-    arrApp = sortElements(arrApp);
-    arrApp.forEach((elLi) => {
-        collection.append(elLi);
-    });
+        //количнство экранов
+        for (let screen of appData.screens) {
+            appData.screens += screen * appData.addScreenBlock;
+        }
+        //возвращает сумму всех дополнительных услуг
+        for (let key in appData.servicesNumbers) {
+            appData.servicePricesNumber += appData.servicesNumbers[key];
+        }
+        //возвращает ghjwtyn
+        for (let key in appData.servicesPercent) {
+            appData.servicePricesPercent +=
+                appData.screenPrice * (appData.servicesPercent[key] / 100);
+        }
+        //возвращает сумму стоимости верстки и стоимости дополнительных услуг
+        appData.fullPrice =
+            appData.screenPrice +
+            appData.servicePricesNumber +
+            appData.servicePricesPercent;
+
+        //метод рассчитывает доход с учетом отката посреднику
+        appData.servicePercentPrice =
+            appData.fullPrice - appData.fullPrice * (appData.rollback / 100);
+    },
+
+    //метод будет выводить в консоль необходимую информацию
+    logger: function() {
+        // Для проерки что ф - я appData работает выводим два значения
+        console.log(appData.fullPrice);
+        console.log(appData.servicePercentPrice);
+        console.log(appData.screens);
+    },
 };
 
-sortChapterLi(books[0].children[1].querySelector("ul"));
-sortChapterLi(books[0].children[4].querySelector("ul"));
-
-//в шестой книге добавить главу “Глава 8: За пределами ES6” и поставить её в правильное место
-let chapter = document.createElement("li");
-chapter.innerText = "Глава 8: За пределами ES6";
-sortChapterLi.books[0].children[5].querySelector("ul").append(chapter);
+appData.init();
